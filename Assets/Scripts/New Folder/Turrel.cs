dc6197ddd8580462e1804b465551d5004efbc60e
DenysY009 <144173746+DenysY009@users.noreply.github.com>
2023-10-31 12:18:23 -0700

    20

--- Assets/Scripts/New Folder/Turrel.cs
+++ Assets/Scripts/New Folder/Turrel.cs
@@ -4,20 +4,31 @@ using System.Collections;
 public class NewMonoBehaviour : MonoBehaviour
 {
     private Vector3 Target; 
-    private float time = 0;
-    private float maxTime = 0;
+    private float timer;
+    private float originalTimer;
+    private GameObject player;
     // Use this for initialization
     void Start()
     {
-
+        player = GameObject.FindWithTag("Player");
+        originalTimer = 6;
+        timer = originalTimer;
+        Target = player.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (timer >= 0)
-        
-           
+        timer -= Time.deltaTime;
+        if (timer < 0)
+        {
+
+
+        }
+        if (Target !=null)
+        {
+            transform.position = Vector3.MoveTowards(current: transform.position, Target, maxDistanceDelta:100);
+        }   
                
                
            
