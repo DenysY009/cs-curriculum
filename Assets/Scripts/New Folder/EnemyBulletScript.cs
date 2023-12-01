d4dd5250c94641caddef48688648c45dfbf94c14
DenysY009 <144173746+DenysY009@users.noreply.github.com>
2023-12-01 12:19:06 -0800

    ghg

---
+++ Assets/Scripts/New Folder/EnemyBulletScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemyBulletScript : MonoBehaviour
+{
+    private GameObject Player;
+    private Rigidbody2D rb;
+    public float force;
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Player = GameObject.FindGameObjectWithTag("Player");
+
+        Vector3 direction = Player.transform.position - transform.position;
+        rb.velocity = new Vector2(direction.x, direction.y).normalized * force;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/New Folder/TurretShuting.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurretShuting : MonoBehaviour
+{
+    public GameObject Projectile;
+    public Transform ProjectilePos;
+    private float timer;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+            timer += Time.deltaTime;
+
+            if(timer>2)
+            {
+                timer = 0;
+                shoot();
+            }
+
+        }
+       
+               void shoot()
+        {
+            Instantiate(Projectile, ProjectilePos.position,Quaternion.identity);
+        }
+           
+        
+    }
+
